44cfd23ea4e2549d4d606e65de1f323cc2e468e1
Alexander Dm. <dabl21291@gmail.com>
2024-09-18 22:08:39 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication18/ConsoleApplication18.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+   
+    int size;
+    cout << "size mass -> ";
+    cin >> size;
+
+    
+    int* array1 = new int[size];
+    cout << "El mass -> ";
+    for (int i = 0; i < size; ++i) {
+        cin >> array1[i];
+    }
+
+   
+    cout << "mass 1 -> ";
+    for (int i = 0; i < size; ++i) {
+        cout << array1[i] << endl;
+    }
+   
+
+    
+    int* array2 = new int[size + 1];
+
+  
+    for (int i = 0; i < size; ++i) {
+        array2[i] = array1[i];
+    }
+
+   
+    array2[size] = 100;
+
+    
+    cout << "mass 2 -> ";
+    for (int i = 0; i < size + 1; ++i) {
+        cout << array2[i] << endl;
+    }
+   
+
+   
+    delete[] array1;
+
+    
+    delete[] array2;
+
+    return 0;
+}
